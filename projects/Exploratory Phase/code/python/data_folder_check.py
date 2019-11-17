51becec0e5801373c8bb90e70ae3132d86cf687e
Daniel Reeves <xdanielreeves@gmail.com>
2019-11-17 18:47:35 -0500

    Fixed error, changed path, added more docuemntation

--- projects/Exploratory Phase/code/python/data_folder_check.py
+++ projects/Exploratory Phase/code/python/data_folder_check.py
@@ -1,9 +1,23 @@
+"""
+This file is a command line interface (CLI) used to assess whether you have all the files you need
+in the data folder. Try it out by typing the following into a terminal window:
+
+```python data_folder_check.py```
+
+The output tells you what files are missing or extra relative to a predefined list.
+
+For additional assistance, type in:
+
+```python data_folder_check.py --help``
+"""
+
 from pprint import pprint
 import click 
 import os
 from collections import namedtuple
 
 FILE_LIST = [
+    'sdwis/README.md',
     'sdwis/contaminant-codes.csv',
     'sdwis/contaminant-group-codes.csv',
     'sdwis/ENFORCEMENT_ACTION.csv',
@@ -15,7 +29,12 @@ FILE_LIST = [
     'sdwis/VIOLATION_ENF_ASSOC.csv',
     'sdwis/VIOLATION.csv',
     'sdwis/WATER_SYSTEM_FACILITY.csv',
-    'sdwis/WATER_SYSTEM.csv'
+    'sdwis/WATER_SYSTEM.csv',
+    'geography/README.md',
+    'geography/water_systems.csv',
+    'geography/water_systems_and_nearest_weather_stations.csv',
+    'geography/weather_stations.csv',
+    'geography/zipcodes.csv',
 ]
 
 OPTIONAL_FILES = []
@@ -96,18 +115,27 @@ py_file_loc = os.path.dirname(os.path.realpath(__file__))
               default=FILE_LIST,
               help="File list to compare against what's in data_dir")
 @click.option('--data_dir',
-              default=os.path.join(py_file_loc, '../../data'),
+              default=os.path.join(py_file_loc, '../../../../data'),
               help='Data directory path.')
 @click.option('--optional_files',
               default=OPTIONAL_FILES,
               help='Files that are not considered extra files but also not required.')
 @click.option('--return_correct',
+              is_flag=True,
               default=False,
               help='Return list of files that match.')
 @click.option('--return_optional',
+              is_flag=True,
               default=False,
               help='Return list of optional files that were found.')
 def cli_run(files, data_dir, optional_files, return_correct, return_optional):
+    """
+    This file is a command line interface (CLI) used to assess whether you have all the files you
+    need in the data folder, compared to a list of required files. This is useful because the
+    Github can't store everything we need, so users have to set up the folder for themselves, and
+    users might be curious if it is consistent with what everyone else is doing. (See the source
+    code for more documentation if needed.)
+    """
     lists = compare_files_and_folders(files=files,
                                       root_dir=data_dir,
                                       optional_files=OPTIONAL_FILES,
